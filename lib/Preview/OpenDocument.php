f819f4ae45d142f39b6f773289bacc99de6861ff
Tor Lillqvist <tml@collabora.com>
2018-08-15 12:19:20 +0300

    The embedded ODF thumbnail in ODF produced by LO (or OOo) is too small
    It is max 256 pixels a side, so let's not bother. I am told Nextcloud
    wants a bigger image for its preview.

--- lib/Preview/OpenDocument.php
+++ lib/Preview/OpenDocument.php
@@ -28,67 +28,6 @@ class OpenDocument extends Office {
 	/**
 	 * {@inheritDoc}
 	 */
-	public function getThumbnail($path, $maxX, $maxY, $scalingup, $fileview) {
-		\OC::$server->getLogger()->debug('=== OpenDocument::getThumbnail: ' . $path);
-
-		$fileInfo = $fileview->getFileInfo($path);
-		if (!$fileInfo) {
-			\OC::$server->getLogger()->debug('... could not get file info');
-			return false;
-		}
-
-		$useTempFile = $fileInfo->isEncrypted() || !$fileInfo->getStorage()->isLocal();
-		if ($useTempFile) {
-			$fileName = $fileview->toTmpFile($path);
-		} else {
-			$fileName = $fileview->getLocalFile($path);
-		}
-		\OC::$server->getLogger()->debug('... file name: ' . $fileName);
-
-		$zip = new \ZipArchive();
-		\OC::$server->getLogger()->debug('... created ZipArchive');
-
-		$res = $zip->open($fileName);
-
-		if ($res !== TRUE) {
-			\OC::$server->getLogger()->debug('... could not open ' . $fileName);
-			return false;
-		}
-		\OC::$server->getLogger()->debug('... opened it');
-
-		$fp = $zip->getStream('Thumbnails/thumbnail.png');
-
-		if (!$fp) {
-			\OC::$server->getLogger()->debug('... no thumbnail? falling back to asking Collabora Online');
-			return Office::getThumbnail($path, $maxX, $maxY, $scalingup, $fileview);
-		}
-
-		$contents = '';
-		while (!feof($fp))
-			$contents .= fread($fp, 10000);
-
-		fclose($fp);
-		\OC::$server->getLogger()->debug('... read ' . strlen($contents) . ' bytes');
-
-		$image = new \OC_Image();
-		$image->loadFromData($contents);
-		\OC::$server->getLogger()->debug('... created and loaded image');
-
-		if ($image->valid()) {
-			\OC::$server->getLogger()->debug('... scaling down');
-			$image->scaleDownToFit($maxX, $maxY);
-
-			\OC::$server->getLogger()->debug('... returning image');
-			return $image;
-		}
-
-		\OC::$server->getLogger()->debug('... returning false');
-		return false;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
 	public function getMimeType() {
 		return '/application\/vnd.oasis.opendocument.*/';
 	}
